9c6dc95b4dd9ff6318e71b4dd4963b5410e16319
Matthew Davis <ml.davis195@gmail.com>
2017-03-10 14:57:07 -0500

    Added some images to projects, fixed matches.php

--- Projects/OnlineRealEstate/PHP/Matches.php
+++ Projects/OnlineRealEstate/PHP/Matches.php
@@ -18,80 +18,29 @@
         <div id="leftPanel" class="sideBanner"> </div>
         <div id="mainPanel">
             <nav id="navPanel"><script>buildNavPanel()</script></nav>
-            <div style="text-align: right">
             <?php
                 require 'Database.php';
-                $query = 'SELECT * FROM user_prefs WHERE user_id='.$_SESSION['user_id'];
-                $result = mysqli_query($con, $query) or die('Unable to access user\'s preferences');
-                $prefs = mysqli_fetch_assoc($result);
-                $query = 'SELECT * FROM properties';
-                $result = mysqli_query($con, $query);
 
-                function hasYesOrNo($pref, $prop, $name) {
-                    if ($pref == 'Any' || $pref == $prop) {
-                        echo $name . ' ok<br/>';
-                    } else {
-                        echo $name . ' not ok<br/>';
-                    }
-                }
+                $query = "SELECT address
+                    FROM user_prefs, properties
+                    WHERE user_prefs.user_id=".$_SESSION['user_id']."
+                    AND properties.city=user_prefs.city
+                    AND properties.cost>=user_prefs.min_cost
+                    AND (properties.cost<=user_prefs.max_cost OR user_prefs.max_cost='5000+')
+                    AND (properties.num_bedrooms=user_prefs.num_bedrooms OR user_prefs.num_bedrooms='Any')
+                    AND (properties.num_bathrooms=user_prefs.num_bathrooms OR user_prefs.num_bathrooms='Any')
+                    AND properties.sq_ft>=user_prefs.min_sq_ft
+                    AND (properties.sq_ft<=user_prefs.max_sq_ft OR user_prefs.max_sq_ft='5000+')
+                    AND (properties.has_balcony=user_prefs.has_balcony OR user_prefs.has_balcony='Any')
+                    AND (properties.is_furnished=user_prefs.is_furnished OR user_prefs.is_furnished='Any')
+                    AND (properties.allows_pets=user_prefs.allows_pets OR user_prefs.allows_pets='Any')
+                    AND (properties.allows_smoking=user_prefs.allows_smoking OR user_prefs.allows_smoking='Any')";
+                $result = mysqli_query($con, $query) or die('Unable to access matches');
 
                 while ($properties = mysqli_fetch_assoc($result)) {
-                    echo $properties['address'] . '<br/>';
-                    if ($properties['city'] == $prefs['city']) {
-                        echo 'city ok<br/>';       
-                    } else {
-                        echo 'city not ok<br>';
-                    }
-
-                    if ($properties['cost'] >= $prefs['min_cost']) {
-                        echo 'min_cost ok<br/>';    
-                    } else {
-                        echo 'min_cost not ok<br/>';
-                    }
-
-                    if ($prefs['max_cost'] == '5000+' || $properties['cost'] <= $prefs['max_cost']) {
-                        echo 'max_cost ok<br/>';    
-                    } else {
-                        echo 'max_cost not ok<br/>';
-                    }
-
-                    if ($prefs['num_bedrooms'] == 'Any'
-                        || $properties['num_bedrooms'] == $prefs['num_bedrooms']) {
-                        echo 'num_bedrooms ok<br/>';
-                    } else {
-                        echo 'num_bedrooms not ok<br/>';
-                    }
-
-                    if ($prefs['num_bathrooms'] == 'Any' 
-                        || $properties['num_bathrooms'] == $prefs['num_bathrooms']) {
-                        echo 'num_bathrooms ok<br/>';
-                    } else {
-                        echo 'num_bathrooms not ok<br/>';
-                    }
-
-                    if ($properties['sq_ft'] >= $prefs['min_sq_ft']) {
-                        echo 'min_sq_ft ok<br/>';
-                    } else {
-                        echo 'min_sq_ft not ok<br/>';
-                    }
-
-                    if ($prefs['min_sq_ft'] == '100'
-                        || $properties['sq_ft'] < $prefs['max_sq_ft']) {
-                        echo 'max_sq_ft ok<br/>';
-                    } else {
-                        echo 'max_sq_ft not ok</br>';
-                    }
-                    
-                    hasYesOrNo($prefs['has_balcony'], $properties['has_balcony'], 'has_balcony');
-                    hasYesOrNo($prefs['has_dishwasher'], $properties['has_dishwasher'], 'has_dishwasher');
-                    hasYesOrNo($prefs['is_furnished'], $properties['is_furnished'], 'is_furnished');
-                    hasYesOrNo($prefs['allows_pets'], $properties['allows_pets'], 'allows_pets');
-                    hasYesOrNo($prefs['allows_smoking'], $properties['allows_smoking'], 'allows_smoking');
-
-                    echo '<br/><br/>';
+                    echo '<div class="strip"><h3>' . $properties['address'] . '</h3></div>';
                 }
-                ?>
-            </div>
+            ?>
         </div>
         <div id="rightPanel" class="sideBanner"> </div>
     </div>
