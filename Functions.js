e2621c6cd327e02ca9e33186b0c1ec65c71646bd
Matthew Davis <ml.davis195@gmail.com>
2017-03-10 22:54:25 -0500

    Added "Online Real Estate" video

--- Functions.js
+++ Functions.js
@@ -1,15 +1,11 @@
 
-function loadProject(id, title, description, uses, githubLink, webLink) {
+function loadProject(id, title, description, uses, githubLink) {
 
     document.write("<div id='" + id + "' class='project'>");
     document.write("<h3>" + title + "</h3>");
     document.write("<p>" + description + "</p>");
     document.write("<p>Uses:" + uses + "</p>");
     document.write("<p>GitHub: <a href='" + githubLink + "'>" + title + "</a></p>");
-
-    if (webLink != undefined) {
-        document.write("<p>Link: <a href='" + webLink + "'>" + title + "</a></p>");
-    }
 
     document.write("<div class='projectVideo'>");
     document.write("<video width='1025' height='576' controls>");
